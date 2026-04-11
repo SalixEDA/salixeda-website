352d40d6c37042fa873ea87e21277a273ed898f3
usik <usiksan@mail.ru>
2026-04-11 19:13:56 +0300

    Complete with features.

--- pages/features.php
+++ pages/features.php
@@ -8,4 +8,25 @@ include 'pages/shorts.php';
 ?>
 
 <style>
+  .block {
+    border: 1px solid #dddddd;
+    padding: 8px;
+    margin-bottom: 12px;
+  }
+
+  h2 {
+    margin-top: 20px;
+    padding-bottom: 3px;
+    border-bottom: 1px solid #cccccc;
+  }
+
+  p {
+    margin-top: 6px;
+    margin-bottom: 6px;
+  }
+
+  ul {
+    margin-top: 5px;
+    margin-bottom: 10px;
+  }
 </style>
